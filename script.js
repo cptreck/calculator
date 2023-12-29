85200bd689db9a1c0ed81505dec3e289ac315087
Jesse Caccavale <58094016+cptreck@users.noreply.github.com>
2023-12-29 14:28:32 -0500

    Create operate function that calls the math functions

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ function divide(num1, num2) {
     num2 = Number(num2); 
     return num1 / num2;
 }
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, operator, num2) {
+    if (operator === "+") {
+       return add(num1, num2);
+    }
+
+    if (operator === "-") {
+        return minus(num1, num2);
+    }
+
+    if (operator === "*") {
+        return multiply(num1, num2);
+    }
+    
+    if (operator === "/") {
+        return divide(num1, num2);
+    }
+}
