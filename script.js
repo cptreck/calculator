fde52e06fc6c6814baa1b6105261db4cef4d43de
Jesse Caccavale <58094016+cptreck@users.noreply.github.com>
2023-12-29 13:59:05 -0500

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2); 
+    return num1 + num2;
+}
+
+function minus(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2); 
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2); 
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2); 
+    return num1 / num2;
+}
