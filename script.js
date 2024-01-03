ae933ece1eaa8ec6ac3e7af98478dc2771f6365a
Jesse Caccavale <58094016+cptreck@users.noreply.github.com>
2024-01-03 15:34:26 -0500

    add if statement for the "C" or reset button

--- script.js
+++ script.js
@@ -45,11 +45,14 @@ function operate(num1, operator, num2) {
     }
 }
 
+const topDisplay = document.querySelector(".calc-top-display");
 const display = document.querySelector('.calc-display');
 const buttons = document.querySelectorAll('button');
+
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         buttonPress = button.value;
+        topDisplay.textContent += display.textContent;
         // possibly change the num1 & num2 variable from taking from the display and get the values from another variable. Gather variable while 
         if ((buttonPress === "+" || button.textContent === "-" || buttonPress === "*" || buttonPress === "/") && num1 === undefined) {
             num1 = display.textContent;
@@ -69,6 +72,15 @@ buttons.forEach((button) => {
             console.log("Im in the second if statment");
         }
 
+        if (button.value === "reset"){
+            topDisplay.textContent = "";
+            display.textContent = "";
+            num1 = undefined;
+            num2 = undefined;
+            operator = undefined;
+            buttonPress = "";
+        }
+        
         display.textContent += buttonPress;
     })
 })
