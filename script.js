442263d4140c89d281658ba0984b197535bb6424
Jesse Caccavale <58094016+cptreck@users.noreply.github.com>
2024-01-03 19:20:10 -0500

    fixed bug where num1 collected the post calculation with the wrong operator

--- script.js
+++ script.js
@@ -65,11 +65,12 @@ buttons.forEach((button) => {
         
         if ((buttonPress === "+" || buttonPress === "-" || buttonPress === "*" || buttonPress === "/") && num2 === undefined) {
             num2 = display.textContent;
+            console.log(num1);
             topDisplay.textContent = operate(num1, operator, num2) + buttonPress;
             display.textContent = "";
+            num1 = operate(num1, operator, num2);  
             operator = buttonPress;
             buttonPress = "";
-            num1 = operate(num1, operator, num2);      
             num2 = undefined;
             console.log("Im in the second if statment");
         }
@@ -91,7 +92,7 @@ buttons.forEach((button) => {
             operator = undefined;
             buttonPress = "";
         }
-        
+        topDisplay.textContent += buttonPress;
         display.textContent += buttonPress;
     })
 })
